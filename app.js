c7467d2a71f5e8fe2bd235cd91fd452a88d80bf3
Danyal Rafiei <Danyal.rs@gmx.de>
2019-07-07 13:23:13 +0200

    server

---
+++ app.js
@@ -0,0 +1,37 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRoutes = require('./routes/index');
+const findRoutes = require('./routes/find');
+const competeRoutes = require('./routes/competition');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',indexRoutes);
+app.use('/find',findRoutes);
+app.use('/compete',competeRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).send(err);
+});
+
+module.exports = app;
